eed98f63573b2de103d152f3439838160ae10b97
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:55 -0500

    xml commenting

--- src/Soenneker.TrustedForm.Runners.OpenApi.Certificates/Constants.cs
+++ src/Soenneker.TrustedForm.Runners.OpenApi.Certificates/Constants.cs
@@ -1,6 +1,12 @@
 ﻿namespace Soenneker.TrustedForm.Runners.OpenApi.Certificates;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.TrustedForm.Certificates.OpenApiClient";
 }
--- src/Soenneker.TrustedForm.Runners.OpenApi.Certificates/Startup.cs
+++ src/Soenneker.TrustedForm.Runners.OpenApi.Certificates/Startup.cs
@@ -13,11 +13,20 @@ namespace Soenneker.TrustedForm.Runners.OpenApi.Certificates;
 public static class Startup
 {
     // This method gets called by the runtime. Use this method to add services to the container.
+    /// <summary>
+    /// Configures services.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
     public static void ConfigureServices(IServiceCollection services)
     {
         services.SetupIoC();
     }
 
+    /// <summary>
+    /// Sets up io c.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The result of the operation.</returns>
     public static IServiceCollection SetupIoC(this IServiceCollection services)
     {
         services.AddHostedService<ConsoleHostedService>()
